2096f7e4d29398d14564bd1bf16d76de07c02778
rex69690 <tusharkoundal913@gmail.com>
2025-02-11 18:01:15 +0530

    routes study and fetching data using multi request , single request

--- QueryParameter and Parents/server.js
+++ QueryParameter and Parents/server.js
@@ -20,6 +20,100 @@ app.get("/about" , (request , response)=>{
     response.send("About me : " + about);
 })
 
+app.get("/contact" , (request , response)=>{
+    const {email , phone } =  request.query;
+    response.send("Contact information : "+email + "  " + phone);
+})
+
+
+//lecture 3rd 
+let userData = [
+    {
+        id: 1,
+        name: "Tushar",
+        address: "Chitkara"
+    },
+    {
+        id: 2,
+        name: "Amit",
+        address: "Delhi"
+    },
+    {
+        id: 3,
+        name: "raj",
+        address: "Delhi"
+    }
+    
+    
+];
+
+//for one single user
+app.get("/info" ,  (request , response)=>{
+
+    const {id} =  request.query;
+
+    for(let i = 0 ; i < userData.length ; i++){
+        if(userData[i].id==id){
+            response.send(userData[i]);
+        }
+        
+    }
+    response.send("not a user")
+   
+
+
+})
+
+//for all users
+app.get("/allusers"  , (request , response)=>{
+    response.send(userData);
+})
+
+
+
+//assignment for blog data
+
+const blog = [
+
+{
+    id : 1,
+    userName : "ram",
+    address : "Chd"
+}, 
+{
+    id : 2 ,
+    userName : "Sam",
+    address : "dlh"
+},
+
+{
+    id : 3,
+    userName : "Rex",
+    address : "hp"
+}
+
+
+];
+
+//for all blog data
+app.get("/blog/all" , (request , response )=>{
+    response.send(blog);
+})
+
+//for single blog data
+
+app.get("/blog/single" ,(request , response)=>{
+    const {id} = request.query;
+
+    for(let i  = 0 ; i < blog.length ; i++){
+        if(blog[i].id==id){
+            response.send(blog[i]);
+        }
+    };
+
+    response.send("Not a user in blog section");
+})
+
 app.listen(3100 , () =>{
     console.log(`server running on ${port}`)
 })
